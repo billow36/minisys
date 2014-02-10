e4159c4a2a3cf797caff2646f89583e746a1daa0
OCEAN <OCEAN@OCEAN-PC>
2014-02-10 10:39:31 +0800

    Test

--- minisys/src/main/java/com/ocean/sys/entity/RbacMenu.java
+++ minisys/src/main/java/com/ocean/sys/entity/RbacMenu.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
@@ -80,7 +80,7 @@ public class RbacMenu {
 	public String getPermission() {
 		return permission;
 	}
-
+	@OrderBy("sort ASC")
 	@Column(name = "SORT")
 	public int getSort() {
 		return sort;
--- minisys/src/test/java/com/ocean/sys/repository/MenuDaoTest.java
+++ minisys/src/test/java/com/ocean/sys/repository/MenuDaoTest.java
@@ -2,6 +2,7 @@ package com.ocean.sys.repository;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,7 +23,26 @@ public class MenuDaoTest extends SpringTransactionalTestCase {
 
 	@Test
 	public void findTasksByUserId() throws Exception {
-		List<RbacMenu> menus = dao.findById(5L);
+		List<RbacMenu> menus = dao.findById(3L);
+		getMenu(menus.get(0));
+		getMenu(menus.get(0));
 		assertEquals(1, menus.size());
+	}
+
+	private void getMenu(RbacMenu rm) {
+		RbacMenu tp = rm.getParent();
+		if (tp != null) {
+			System.out.println("父目录：" + rm.getParent().getMenuName());
+		}
+		else{
+			System.out.println("根目录");
+		}
+		System.out.println("当前目录：" + rm.getMenuName());
+
+		Iterator<RbacMenu> it = rm.getChilds().iterator();
+
+		while (it.hasNext()) {
+			getMenu((RbacMenu) it.next());
+		}
 	}
 }
